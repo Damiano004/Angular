6e3c05feddbffe82f94582105e2790a607fc32e2
Mael Ossanna Thimon <Mael.OssannaThimon@marconi.loc>
2025-02-06 10:39:06 +0100

    Aggiunta una chiamata HTTP nel programma che mostri dei post inviati tramite get

--- Fake-Instagram/src/app/Core/post-manager/post-manager.service.ts
+++ Fake-Instagram/src/app/Core/post-manager/post-manager.service.ts
@@ -1,19 +1,31 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { Post } from '../Model/Post.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostManagerService {
+  #URL = 'https://jsonplaceholder.typicode.com/posts';
+
+  #http = inject(HttpClient);
+
   //Con la dicitura "#", si rende la variabile privata, rendendola inaccessibile
   //da fuori dalla classe postManagerService
   #postList = signal<Post[]>([]);
 
   //è come un signal ma, a differenza di #postList, è un signal solamente
   //leggibile e non anche scrivibile.
-  posetListComp = computed(() => this.#postList())
+  posetListComp = computed(() => this.#postList());
 
   constructor() { }
+
+  recuperaPostViaHTTP(): void{
+    this.#http.get<Post[]>(this.#URL).subscribe((postList: Post[]) => {
+      console.log(postList);
+      this.#postList.set(postList);
+    });
+  }
 
   //negli array, utilizzando lo spread operator "..." esporta una copia du tutti i
   //valori dell'array, è come ciclare per tutto l'array che fa "array1[i] = array2[i]"
