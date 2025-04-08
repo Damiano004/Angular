5fb676bbc64cbe5e35f4e201095cc1d34e922845
Mael Ossanna Thimon <Mael.OssannaThimon@marconi.loc>
2025-04-08 11:23:58 +0200

    ha fatto delle cose con le form, prova a vedere se riesci a sistemare (tralaltro pernso che il problema sia principalmente per il fatto che faccoi tutto in una pagina)

--- AppOspedale/AFPOspedale/src/app/Features/accetta-pz/accetta-pz.component.ts
+++ AppOspedale/AFPOspedale/src/app/Features/accetta-pz/accetta-pz.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { CodiceCOlore, StatoPaziete } from '../../Core/Models/Paziente';
+import { FormsModule } from '@angular/forms';
+import { AFPOspedaleAPIService } from '../../Core/Service/AFPOspedaleAPI.service';
 
 @Component({
   selector: 'app-accetta-pz',
+  imports: [FormsModule],
   templateUrl: './accetta-pz.component.html',
   styleUrls: ['./accetta-pz.component.scss']
 })
-export class AccettaPzComponent implements OnInit {
+export class AccettaPzComponent {
+  readonly #api = inject(AFPOspedaleAPIService);
 
-  constructor() { }
+  readonly nome = signal<string>('');
+  readonly cognome = signal<string>('');
+  readonly dataNascita = signal<string>('');
+  readonly dataNascitaParse = computed(() =>(
+    this.dataNascita() ? new Date(this.dataNascita()) : new Date('1970-01-01'))
+  );
+  readonly codiceFiscale = signal<string>('');
+  readonly codiceColore = signal<CodiceCOlore>('NON FORNITO');
+  readonly codicePaziente = signal<string>('');
+  readonly statoPaziente = signal<StatoPaziete>('NON FORNITO');
 
-  ngOnInit() {
+  accettaPaziente(): void{
+
   }
 
+  validateInput(): boolean{
+    if(!this.nome()) return false;
+    if(!this.cognome()) return false;
+    if(
+      !this.codiceFiscale()||
+      this.codiceFiscale.length !== 16
+    ) return false;
+    if(this.codiceColore() ==='NON FORNITO') return false;
+    if(!this.nome()) return false;
+    //if(!this.dataNascitaParse()) return false;
+    return true;
+  }
 }
